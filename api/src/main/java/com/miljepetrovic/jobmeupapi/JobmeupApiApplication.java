9d7f94551f0182ca22f07271399f3a1f38bc0550
Milan Petrovic <milanpdo@gmail.com>
2020-09-20 11:34:58 +0200

    Initial commit

---
+++ api/src/main/java/com/miljepetrovic/jobmeupapi/JobmeupApiApplication.java
@@ -0,0 +1,13 @@
+package com.miljepetrovic.jobmeupapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobmeupApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JobmeupApiApplication.class, args);
+	}
+
+}
---
+++ api/src/test/java/com/miljepetrovic/jobmeupapi/JobmeupApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.miljepetrovic.jobmeupapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobmeupApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
